1848988c07dadb902575b41a675c4e2c36a59215
Isaac <isaacarag@uamv.edu.ni>
2024-06-09 15:25:20 -0600

    Sistema de PIN
    Actualización del sistema de PIN. En lugar de recorrer el arreglo de ´pines[]´ comprueba con ´if´ y ´else if´ individualmente.

--- cajero.cpp
+++ cajero.cpp
@@ -1,24 +1,46 @@
-/*Automatización de servicios bancarios mediante un cajero automático*/
+//PIN
 #include <stdio.h>
 int main()
 {
-    int pins[] = {1234, 4321, 1111, 9999};
-    int input_pin;
-    int valid_pins = 4;
-    int i;
-    printf("Ingrese su PIN");
-    scanf("%d",&input_pin);
-
-    for(i = 0; i < valid_pins; i++)
+    int intentos = 3, pincorrecto, pines[] = {1234, 2345, 3456, 4567};
+    printf("Bienvenido al cajero automático\n");
+    do
     {
-        pins[i];
-    }
-    if (pins[0,1,2,3])
+        printf("\nNúmero de intentos disponibles: %d", intentos);
+        printf("\nInserte su PIN:\n\n");
+        scanf("%d", &pincorrecto);
+        if (pincorrecto == pines[0])
+        {
+            intentos = -1;
+            break;
+        }
+        else if (pincorrecto == pines[1])
+        {
+            intentos = -1;
+            break;
+        }
+        else if (pincorrecto == pines[2])
+        {
+            intentos = -1;
+            break;
+        }
+        else if (pincorrecto == pines[3])
+        {
+            intentos = -1;
+            break;
+        }
+        else
+        {
+            intentos-= 1;
+            printf("Pin incorrecto.\n");
+        }
+        
+    } while (intentos != 0);
+    if (intentos == 0)
     {
-        printf("\nBienvenido");
+        printf("Su usuario ha sido bloqueado");
+        return 0;
     }
-    else
-    {
-        printf ("\nIncorrecto. Intente nuevamente");
-    }
+    printf("Acceso concedido");
+    return 0;
 }
